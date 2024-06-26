f30bb9e3629c8246e5da7646f87bcb298e94fab1
Nagarjun A <nagarjun.avala.official@gmail.com>
2024-06-26 13:37:32 +0530

    Final commit_

--- app.js
+++ app.js
@@ -10,25 +10,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// Testing Routes
-// app.get("/", (req, res) => {
-//   res.status(200).send("Hello");
-// });
-// app.get("/task", (req, res) => {
-//   const issue = {
-//     title: "Task creation",
-//     body: "This is a test task",
-//     user: {
-//       login: "advaith",
-//     },
-//     html_url: "https://github.com/advaith/asana-github-integration/issues/1",
-//   };
-//   if (issue) {
-//     createAsanaTask(issue);
-//   }
-//   res.status(200).send("OK");
-// });
-
 app.post("/webhook", (req, res) => {
   const signature = req.headers["x-hub-signature-256"];
 
@@ -64,7 +45,7 @@ function createAsanaTask(issue) {
     external: {
       data: issue.html_url,
     },
-    // assignee: getAsanaUser(issue.user.login),
+    assignee: getAsanaUser(issue.user.login),
   };
 
   let client = Asana.ApiClient.instance;
@@ -72,7 +53,7 @@ function createAsanaTask(issue) {
   token.accessToken = process.env.ASANA_ACCESS_TOKEN;
 
   let tasksApiInstance = new Asana.TasksApi();
-  let body = { data }; // Object | The task to create.
+  let body = { data };
   let opts = {
     opt_fields:
       "actual_time_minutes,approval_status,assignee,assignee.name,assignee_section,assignee_section.name,assignee_status,completed,completed_at,completed_by,completed_by.name,created_at,created_by,custom_fields,custom_fields.asana_created_field,custom_fields.created_by,custom_fields.created_by.name,custom_fields.currency_code,custom_fields.custom_label,custom_fields.custom_label_position,custom_fields.date_value,custom_fields.date_value.date,custom_fields.date_value.date_time,custom_fields.description,custom_fields.display_value,custom_fields.enabled,custom_fields.enum_options,custom_fields.enum_options.color,custom_fields.enum_options.enabled,custom_fields.enum_options.name,custom_fields.enum_value,custom_fields.enum_value.color,custom_fields.enum_value.enabled,custom_fields.enum_value.name,custom_fields.format,custom_fields.has_notifications_enabled,custom_fields.id_prefix,custom_fields.is_formula_field,custom_fields.is_global_to_workspace,custom_fields.is_value_read_only,custom_fields.multi_enum_values,custom_fields.multi_enum_values.color,custom_fields.multi_enum_values.enabled,custom_fields.multi_enum_values.name,custom_fields.name,custom_fields.number_value,custom_fields.people_value,custom_fields.people_value.name,custom_fields.precision,custom_fields.representation_type,custom_fields.resource_subtype,custom_fields.text_value,custom_fields.type,dependencies,dependents,due_at,due_on,external,external.data,followers,followers.name,hearted,hearts,hearts.user,hearts.user.name,html_notes,is_rendered_as_separator,liked,likes,likes.user,likes.user.name,memberships,memberships.project,memberships.project.name,memberships.section,memberships.section.name,modified_at,name,notes,num_hearts,num_likes,num_subtasks,parent,parent.created_by,parent.name,parent.resource_subtype,permalink_url,projects,projects.name,resource_subtype,start_at,start_on,tags,tags.name,workspace,workspace.name",
